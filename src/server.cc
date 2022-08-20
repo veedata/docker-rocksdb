115475ac22db0e62564440f86816512e00f13eea
Viraj Thakkar <43007623+veedata@users.noreply.github.com>
2022-08-20 01:37:30 -0700

    testing port

--- src/server.cc
+++ src/server.cc
@@ -29,7 +29,7 @@ using ROCKSDB_NAMESPACE::Status;
 using ROCKSDB_NAMESPACE::WriteOptions;
 
 
-const std::string hdfsEnv = "hdfs://hdfs-node:9000/";
+const std::string hdfsEnv = "hdfs://172.17.0.2:9000/";
 const std::string kDBPrimaryPath = "primary";
 // const std::string kDBSecondaryPath = "vee/ported/sec";
 
