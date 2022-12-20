8edcff923276455e3ed2a2c7fe202a5028bcc4fc
Viraj Thakkar <43007623+veedata@users.noreply.github.com>
2022-12-20 13:18:40 -0700

    close conn debug

--- src/server.cc
+++ src/server.cc
@@ -184,7 +184,7 @@ int CheckConnections() {
 
                 //Somebody disconnected , get his details and print 
                 getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);  
-                // printf("Host disconnected , ip %s , port %d \n", inet_ntoa(address.sin_addr) , ntohs(address.sin_port));  
+                printf("Host disconnected , ip %s , port %d \n", inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
 
                 //Close the socket and mark as 0 in list for reuse 
                 close( sd );  
